30d26b8b586ed3db27ef72b1073199b0055d5685
Torben Gallob <tgallob@anexia-it.com>
2020-05-10 15:45:13 +0200

    addes prisma js

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello(): Promise<string> {
+
+    const prisma = new PrismaClient();
+
+    const users = await prisma.entry.findMany();
+
+    return users.toString();
   }
 }
